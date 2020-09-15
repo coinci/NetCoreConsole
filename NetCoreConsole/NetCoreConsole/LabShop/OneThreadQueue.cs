e260c1b2094c1b7b389fb23a539cf482931cf15f
Jeff.Wang <dayan13@live.cn>
2020-09-15 23:16:36 +0800

    init

---
+++ NetCoreConsole/NetCoreConsole/LabShop/OneThreadQueue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoreConsole.LabShop
+{
+    public class OneThreadQueue
+    {
+        public ConcurrentQueue<string> CurrentQueue = new ConcurrentQueue<string>();
+
+        public OneThreadQueue()
+        {
+            StartProcess();
+        }
+
+        public void AddItem(string item)
+        {
+            CurrentQueue.Enqueue(item);
+        }
+
+        public void StartProcess()
+        {
+            Task.Factory.StartNew(HandleQueue);
+        }
+
+        public void HandleQueue()
+        {
+            while (true)
+            {
+                string currentItem = string.Empty;
+                if (CurrentQueue.TryDequeue(out currentItem))
+                {
+                    Console.WriteLine("Processing");
+                    Task.Delay(1000).Wait();
+                    Console.WriteLine(currentItem);
+                }
+
+                Console.WriteLine("Starting wait empty!!");
+                Task.Delay(200).Wait();
+            }
+        }
+
+    }
+}
---
+++ NetCoreConsole/NetCoreConsole/Program.cs
@@ -0,0 +1,56 @@
+﻿using NetCoreConsole.LabShop;
+using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace NetCoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            try
+            {
+                TryQueue();
+                // TryException(true).Wait();
+
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+
+        }
+
+        public static void TryQueue()
+        {
+            OneThreadQueue queue = new OneThreadQueue();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var delay = new Random(DateTime.Now.Millisecond%100).Next(150);
+                Task.Delay(delay*10).Wait();
+                queue.AddItem($"#{i+1} : current delay{delay}");
+            }
+        }
+
+        public static async Task<string> TryException(bool flag)
+        {
+            string s1 = "some new line 1";
+            string s2 = "Some new line 2";
+
+            await Task.Delay(1000);
+            if (flag)
+            {
+                throw new Exception("Exception inside occurs.");
+            }
+
+
+            string result = s1 + s2;
+            return result;
+        }
+    }
+}
